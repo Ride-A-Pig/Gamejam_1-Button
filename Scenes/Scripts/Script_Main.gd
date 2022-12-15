4cab4f5f82b9a591e6948a31df72074e702bdbc4
Ride_A_Pig <marthxia@gmail.com>
2022-12-14 19:32:23 -0700

    Update Script_Main.gd

--- Scenes/Scripts/Script_Main.gd
+++ Scenes/Scripts/Script_Main.gd
@@ -4,7 +4,7 @@ onready var timer = $TimeLable
 var time:int = 0
 
 func _input(event):
-	if event.is_action_pressed("ui_accept") and $ColorRect:
+	if event.is_action_pressed("ui_accept") and get_node_or_null("ColorRect"):
 		$ColorRect.queue_free()
 		$TimeLable/Timer.start()
 		$PopupLayer/Intro6.active = true
